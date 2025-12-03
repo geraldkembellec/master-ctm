e70c6d4d7d9c4cdec64a7c69f7ad645c49e9fe5f
Kembellec <geraldkembellec@gmail.com>
2025-12-03 19:09:35 +0100

    Add files via upload

---
+++ leaflet/mespetitesfonctions.js
@@ -0,0 +1,21 @@
+function loadLeafletMap(){
+		const lien = "https://www.pantheonsorbonne.fr/";
+		const map = L.map('map').setView([48.8271, 2.3646], 15);
+		const tiles = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+			maxZoom: 19,
+			attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+		}).addTo(map);
+
+		const marker = L.marker([48.8271, 2.3646]).addTo(map)
+			.bindPopup('<img src="https://upload.wikimedia.org/wikipedia/fr/4/42/CentrePMF_Paris1.jpg" width="100%"><a href="https://www.pantheonsorbonne.fr/"><strong>Université PARIS 1</strong></a><br>Site PMF (Tolbiac).').openPopup();
+	}
+
+
+	function onMapClick(e) {
+		popup
+			.setLatLng(e.latlng)
+			.setContent(`You clicked the map at ${e.latlng.toString()}`)
+			.openOn(map);
+	}
+
+	map.on('click', onMapClick);
