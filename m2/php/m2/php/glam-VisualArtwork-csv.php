e1e0a678ed7fc63746dfa5b9bf7b63498e935a4d
Kembellec <geraldkembellec@gmail.com>
2026-04-23 12:08:36 +0200

    Update glam-VisualArtwork-csv.php

--- m2/php/m2/php/glam-VisualArtwork-csv.php
+++ m2/php/m2/php/glam-VisualArtwork-csv.php
@@ -43,96 +43,82 @@ if (($handle = fopen($csvFile, "r")) !== false) {
 
 <!DOCTYPE html>
 <html lang="fr">
-  <head>
+    <head>
     <meta charset="UTF-8">
-    <title>Œuvres visuelles</title>
-  </head>
+        <title>Œuvres visuelles</title>
+    </head>
+    <body>
+        <h1>Œuvres visuelles</h1>
+        <?php foreach ($artworks as $artwork): ?>
+        <article
+            itemscope
+            itemtype="https://schema.org/VisualArtwork"
+            itemid="https://www.wikidata.org/wiki/<?= htmlspecialchars($artwork['id_wikidata'], ENT_QUOTES, 'UTF-8') ?>"
+        >
+        <meta
+            itemprop="identifier"
+            content="https://www.wikidata.org/wiki/<?= htmlspecialchars($artwork['id_wikidata'], ENT_QUOTES, 'UTF-8') ?>"
+        >
+        <?php if ($img = wikimediaImageUrl($artwork['image_filename'])): ?>
+        <img
+        src="<?= htmlspecialchars($img, ENT_QUOTES, 'UTF-8') ?>"
+        alt="<?= htmlspecialchars($artwork['title'], ENT_QUOTES, 'UTF-8') ?>"
+        width="80%"
+        itemprop="image"
+        >
+        <?php endif; ?>
+        <h2 itemprop="name">
+            <?= htmlspecialchars($artwork['title'], ENT_QUOTES, 'UTF-8') ?>
+        </h2>
 
-<body>
-  <h1>Œuvres visuelles</h1>
-  <?php foreach ($artworks as $artwork): ?>
-
-    <article
-    itemscope
-    itemtype="https://schema.org/VisualArtwork"
-    itemid="https://www.wikidata.org/wiki/<?= htmlspecialchars($artwork['id_wikidata'], ENT_QUOTES, 'UTF-8') ?>"
-    >
-    <meta
-    itemprop="identifier"
-    content="https://www.wikidata.org/wiki/<?= htmlspecialchars($artwork['id_wikidata'], ENT_QUOTES, 'UTF-8') ?>"
-    >
-
-    <?php if ($img = wikimediaImageUrl($artwork['image_filename'])): ?>
-
-    <img
-    src="<?= htmlspecialchars($img, ENT_QUOTES, 'UTF-8') ?>"
-    alt="<?= htmlspecialchars($artwork['title'], ENT_QUOTES, 'UTF-8') ?>"
-    width="80%"
-    itemprop="image"
-    >
-
-    <?php endif; ?>
-
-    <h2 itemprop="name">
-    <?= htmlspecialchars($artwork['title'], ENT_QUOTES, 'UTF-8') ?>
-    </h2>
-
-    <p itemprop="description">
-    <?= htmlspecialchars($artwork['description'], ENT_QUOTES, 'UTF-8') ?>
-    </p>
-
-    <div>
-      <strong>Auteur :</strong>
-      <p
-      itemscope
-      itemtype="https://schema.org/Person"
-      itemprop="creator"
-      >
-        <span itemprop="name">
-        <?= htmlspecialchars($artwork['creator'], ENT_QUOTES, 'UTF-8') ?>
-        </span>
-      </p>
-    </div>
-      
-    <p>
-      <strong>Date de création :</strong>
-      <span itemprop="dateCreated">
-      <?= htmlspecialchars($artwork['creation_date'], ENT_QUOTES, 'UTF-8') ?>
-      </span>
-    </p>
-
-    <p>
-      <strong>Médium :</strong>
-      <span itemprop="artMedium">
-      <?= htmlspecialchars($artwork['art_medium'], ENT_QUOTES, 'UTF-8') ?>
-      </span>
-    </p>
-
-    <p>
-      <strong>Type d’œuvre :</strong>
-      <span itemprop="artform">
-      <?= htmlspecialchars($artwork['artform'], ENT_QUOTES, 'UTF-8') ?>
-      </span>
-    </p>
-
-    <p>
-      <strong>Lieu de conservation :</strong>
-      <span itemprop="contentLocation">
-      <?= htmlspecialchars($artwork['location'], ENT_QUOTES, 'UTF-8') ?>
-      </span>
-    </p>
-    <p>
-      <a
-      href="https://www.wikidata.org/wiki/<?= htmlspecialchars($artwork['id_wikidata'], ENT_QUOTES, 'UTF-8') ?>"
-      target="_blank"
-      rel="noopener"
-      >
-      Référence Wikidata
-      </a>
-    </p>
-</article>
-<hr>
-<?php endforeach; ?>
-
+        <p itemprop="description">
+            <?= htmlspecialchars($artwork['description'], ENT_QUOTES, 'UTF-8') ?>
+        </p>
+        <div>
+          <strong>Auteur :</strong>
+          <p itemscope
+          itemtype="https://schema.org/Person"
+          itemprop="creator">
+            <span itemprop="name">
+                <?= htmlspecialchars($artwork['creator'], ENT_QUOTES, 'UTF-8') ?>
+            </span>
+          </p>
+        </div>
+        <p>
+          <strong>Date de création :</strong>
+          <span itemprop="dateCreated">
+          <?= htmlspecialchars($artwork['creation_date'], ENT_QUOTES, 'UTF-8') ?>
+          </span>
+        </p>
+        <p>
+          <strong>Médium :</strong>
+          <span itemprop="artMedium">
+          <?= htmlspecialchars($artwork['art_medium'], ENT_QUOTES, 'UTF-8') ?>
+          </span>
+        </p>
+        <p>
+          <strong>Type d’œuvre :</strong>
+          <span itemprop="artform">
+          <?= htmlspecialchars($artwork['artform'], ENT_QUOTES, 'UTF-8') ?>
+          </span>
+        </p>
+        <p>
+          <strong>Lieu de conservation :</strong>
+          <span itemprop="contentLocation">
+          <?= htmlspecialchars($artwork['location'], ENT_QUOTES, 'UTF-8') ?>
+          </span>
+        </p>
+        <p>
+          <a
+          href="https://www.wikidata.org/wiki/<?= htmlspecialchars($artwork['id_wikidata'], ENT_QUOTES, 'UTF-8') ?>"
+          target="_blank"
+          rel="noopener"
+          >
+          Référence Wikidata
+          </a>
+        </p>
+    </article>
+    <hr>
+    <?php endforeach; ?>
 </body>
 </html>
